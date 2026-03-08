92e0cb9fc2727a0cf98175c00c4b8dbc6038c4ad
okakdoza0-rgb <okakdoza0@gmail.com>
2026-03-08 22:18:29 +0200

    Update Telegram bot token and message format

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.8407383766:AAECu0jVu9Up7R-N3MehalGHJk-XYrSrtWY
-
-;
+const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(
-    msg.chat.id,
+
+bot.sendMessage(msg.chat.id,
 `👋 Добро пожаловать в ArTap!
 
 👆 Тапай Artemwe
@@ -17,7 +15,8 @@ bot.onText(/\/start/, (msg) => {
 🏆 Стань лучшим игроком
 
 Начни тапать прямо сейчас 🚀`
-  );
+);
+
 });
 
-console.log('Bot started');
+console.log("Bot started");
