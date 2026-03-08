1ed2f45574bab09b6af1dc5eb934fac5da3314f3
okakdoza0-rgb <okakdoza0@gmail.com>
2026-03-08 18:23:49 +0200

    Add Telegram bot initialization and start command

--- bot.js
+++ bot.js
@@ -1,1 +1,8 @@
+const TelegramBot = require('node-telegram-bot-api');
 
+const token = 'TOKEN_БОТА';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "👆 Тапай Artemwe\n🪙 Зарабатывай монеты\n⚡ Улучшай силу");
+});
