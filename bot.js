36b974fb8cdc5f645ed3fbc15d4a3832dfb97025
okakdoza0-rgb <okakdoza0@gmail.com>
2026-03-09 07:41:26 +0200

    Replace hardcoded token with environment variable

--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '8668448787:AAH2w4EsedEnRMXXoWchIWubQAeIPTPZt5Q';
+const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id,
+  bot.sendMessage(
+    msg.chat.id,
 `🎮 Artemwe Tap
 
-👆 Тапай и зарабатывай монеты
-🪙 Копи баланс
+👇 Тапай и зарабатывай монеты
+🌑 Копи баланс
 ⚡ Улучшай силу
 🏆 Стань лучшим игроком`
-);
+  );
 });
