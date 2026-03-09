88842a4869c72017ca0b7e4a6c03414638c49e2c
okakdoza0-rgb <okakdoza0@gmail.com>
2026-03-09 09:38:32 +0200

    Enhance bot.js with start command and server setup

--- bot.js
+++ bot.js
@@ -1,18 +1,50 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
+// команда /start
 bot.onText(/\/start/, (msg) => {
+
   bot.sendMessage(
     msg.chat.id,
 `🎮 Добро пожаловать в *ArTap!*
 
 👆 Тапай по Artemwe  
-💰 Зарабатывай монеты  
+🪙 Зарабатывай монеты  
 ⚡ Прокачивай силу клика  
 🏆 Стань лучшим игроком  
 
-🚀 Нажми «Играть» и начинай тапать!`
-  );
+🚀 Нажми «Играть» и начинай тапать!`,
+{
+parse_mode: "Markdown",
+reply_markup: {
+inline_keyboard: [
+[
+{
+text: "🎮 Играть",
+web_app: {
+url: "https://tap-game-oxjs.onrender.com"
+}
+}
+]
+]
+}
+}
+);
+
+});
+
+// сервер для Render
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
