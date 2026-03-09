0b889cccd02934b50d59a930a382554918142b6c
okakdoza0-rgb <okakdoza0@gmail.com>
2026-03-09 21:34:01 +0200

    Refactor bot.js to streamline commands and responses

--- bot.js
+++ bot.js
@@ -4,65 +4,11 @@ const express = require('express');
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-const app = express();
+// команда /start
+bot.onText(/\/start/, (msg) => {
 
-app.use(express.json());
-
-/*
-Храним пользователей
-id -> true
-*/
-const users = {};
-
-/*
-Реферальные награды
-*/
-const rewards = {};
-
-/* /start */
-bot.onText(/\/start(?: (.+))?/, (msg, match) => {
-
-const chatId = msg.chat.id;
-const username = msg.from.username || msg.from.first_name;
-
-const referrer = match[1];
-
-/*
-Проверяем новый ли игрок
-*/
-const isNewUser = !users[chatId];
-
-if (isNewUser) {
-
-users[chatId] = true;
-
-/*
-Если есть реферал
-*/
-if (referrer && referrer != chatId) {
-
-if (!rewards[referrer]) {
-rewards[referrer] = 0;
-}
-
-rewards[referrer] += 1000;
-
-/*
-Сообщение пригласившему
-*/
-bot.sendMessage(referrer,
-`🎉 Новый игрок перешёл по твоей ссылке!
-
-Игрок: @${username}
-
-🪙 Ты получил 1000`
-);
-
-}
-
-}
-
-bot.sendMessage(chatId,
+  bot.sendMessage(
+    msg.chat.id,
 `🎮 Добро пожаловать в *ArTap!*
 
 👆 Тапай по Artemwe  
@@ -71,68 +17,22 @@ bot.sendMessage(chatId,
 🏆 Стань лучшим игроком  
 
 🚀 Начни играть прямо сейчас!`,
-{ parse_mode: "Markdown" });
-
-});
-
-
-/* команда пригласить */
-bot.onText(/\/invite/, (msg) => {
-
-const chatId = msg.chat.id;
-
-const link = `https://t.me/ArTapclicker_bot?start=${chatId}`;
-
-bot.sendMessage(chatId,
-`👥 Пригласи друзей
-
-Твоя ссылка:
-
-${link}
-
-За каждого друга +1000 🪙`
+{
+parse_mode: "Markdown"
+}
 );
 
 });
 
+// сервер для Render (чтобы Render не выключал бота)
+const app = express();
 
-/* API для игры */
-app.get('/reward/:id', (req, res) => {
-
-const id = req.params.id;
-
-const reward = rewards[id] || 0;
-
-res.json({
-reward
-});
-
-});
-
-
-/* забрать награду */
-app.post('/claim', (req, res) => {
-
-const id = req.body.id;
-
-const reward = rewards[id] || 0;
-
-rewards[id] = 0;
-
-res.json({
-reward
-});
-
-});
-
-
-/* сервер */
 app.get('/', (req, res) => {
-res.send('Bot running');
+  res.send('Bot is running');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-console.log("Server running");
+  console.log("Server running on port " + PORT);
 });
