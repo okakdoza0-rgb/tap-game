390b43cb99d3adc8eaa7b5a1c67b6f8d5bfec45e
okakdoza0-rgb <okakdoza0@gmail.com>
2026-03-11 20:27:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,17 @@ const express = require('express');
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
+// список игроков
+let users = new Set();
+
+// твой Telegram ID (замени на свой)
+const adminId = 7837011810;
+
 // команда /start
 bot.onText(/\/start/, (msg) => {
+
+  // сохраняем игрока
+  users.add(msg.from.id);
 
   bot.sendMessage(
     msg.chat.id,
@@ -22,6 +31,13 @@ parse_mode: "Markdown"
 }
 );
 
+});
+
+// команда чтобы узнать игроков
+bot.onText(/\/players/, (msg) => {
+  if (msg.from.id === adminId) {
+    bot.sendMessage(msg.chat.id, "👥 Игроков: " + users.size);
+  }
 });
 
 // сервер для Render (чтобы Render не выключал бота)
